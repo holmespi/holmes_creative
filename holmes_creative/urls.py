bbd3baad805c9368b5520bb00e71b9d93e171598
holmespi <holmespi90@gmail.com>
2014-03-08 01:50:11 -0500

    style changes | favicon setup

--- holmes_creative/urls.py
+++ holmes_creative/urls.py
@@ -16,6 +16,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
+    (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/img/favicon.ico'}),
+
     url(r'^$', views.home, name='home'),
 
     url(r'^sketchbook/', views.sketchbook, name='sketchbook'),
