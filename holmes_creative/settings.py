72099136f1d39543131098bdd1f81d257671fae4
holmespi <holmespi90@gmail.com>
2014-03-08 16:27:13 -0500

    uninstalled south again f, it

--- holmes_creative/settings.py
+++ holmes_creative/settings.py
@@ -145,7 +145,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'south',
     'gunicorn',
     'portfolio',
 )
